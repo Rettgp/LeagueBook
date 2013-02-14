a2ad2efeeecd1655bda50c730113aa3c3382d2fd
Rytael <garephelps@live.com>
2013-02-13 22:06:22 -0800

    Added login for home page

---
+++ scripts/authentication.js
@@ -0,0 +1,8 @@
+function authenticate(acct, pass){
+	alert(acct + " " + pass);
+	if(acct == "rytael" && pass == "nigger12"){
+		alert("FUCK YOU!");
+		window.location = "file:///C:/Users/Garrett's/Desktop/Baylife/trunk/userhomepage/userhomepage.html";
+	}else
+		alert("NOODLES");
+}
